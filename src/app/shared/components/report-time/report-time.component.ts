027bd85cbb0316648fe5ddbc26e1f22e7bbb276b
Tomy97 <64372041+Tomy97@users.noreply.github.com>
2021-08-05 05:36:27 -0300

    Mock de las temperaturas  y estilos de cada 1

--- src/app/shared/components/report-time/report-time.component.ts
+++ src/app/shared/components/report-time/report-time.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportTimeComponent implements OnInit {
   title: string = 'Reporte del Tiempo'
-  constructor() { }
-
+  pais: string = 'Argentina'
+  ciudad: string = 'Ciudad Autonoma de Buenos Aires'
+  day: string = 'Viernes'
+  stay: string = 'Soleado'
+  tempertureC: number = 22
+  tempertureF: number = 71.6
+  constructor() {
+    console.log(this.tempertureF);
+  }
   ngOnInit(): void {
   }
-
+  
 }
